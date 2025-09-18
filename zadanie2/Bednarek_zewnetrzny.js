e177291b44836b1a33f7ee3a2e3463876e4eb752
Wojciechyaho510 <wojtekbgames@gmail.com>
2025-09-18 22:17:30 +0200

    Add files via upload

---
+++ zadanie2/Bednarek_zewnetrzny.js
@@ -0,0 +1,6 @@
+document.write("Mój numer w dzienniku to 1 <br>");
+document.write("Pierwszy wiersz to:"+"&nbsp;16&nbsp;&nbsp;Tak&nbsp;&nbsp;Nie&nbsp;&nbsp;6 <br>");
+document.write("Drugi Wiersz to:"+"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;13&nbsp;&nbsp;Tak&nbsp;&nbsp;Nie&nbsp;&nbsp;6 <br>");
+
+document.write("ostatnia modyfikacja strony".fontcolor("aqua").bold().fontsize(6)+"<br>"); 
+document.write(document.lastModified.fontcolor("navy").bold().fontsize(6)); 
